56c754641e9dff86f9b4d364a64b2bef3567c44e
gabelm344 <113635032+gabelm344@users.noreply.github.com>
2024-11-26 14:13:13 -0500

    TestAddBook Fail
    TestAddBook will fail because I changed it to add a User instead of a Book

--- Lab 6/UnitTest1.cs
+++ Lab 6/UnitTest1.cs
@@ -18,7 +18,7 @@ public class LibraryServiceTests
 
         var book = new Book { Title = "Test Book", Author = "Author", ISBN = "12345" };
 
-        _libraryService.AddBook(book);
+        _libraryService.AddBook(user);
 
         var books = _libraryService.GetBooks();
         Assert.AreEqual(1, books.Count, "Book count should be 1.");
